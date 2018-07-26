0ca4af8667b39c20416d52f6f63775082a4446bf
kylincat <1059914928@qq.com>
2018-07-26 23:10:45 +0800

    完成整体布局

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+
+
+function changeColor1() {
+		var s1 = document.getElementById("s1");
+var p1 = document.getElementById("p1");
+var s2 = document.getElementById("s2");
+var p2 = document.getElementById("p2");
+		s1.style.color = 'blue';
+		p1.style.color = 'blue';
+		s2.style.color = 'rgb(177, 156, 156)';
+		p2.style.color = 'rgb(177, 156, 156)';
+		
+}
+function changeColor2() {
+	var s1 = document.getElementById("s1");
+var p1 = document.getElementById("p1");
+var s2 = document.getElementById("s2");
+var p2 = document.getElementById("p2");
+		s1.style.color = 'rgb(177, 156, 156)';
+		p1.style.color = 'rgb(177, 156, 156)';
+		s2.style.color = 'blue';
+		p2.style.color = 'blue';
+}
