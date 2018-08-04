47c08fc91770c07bcc1707e562a2fe6ea1ccab5b
kylincat <1059914928@qq.com>
2018-08-04 14:25:07 +0800

    ”Jquery实现图片无缝轮播

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@
 
 function changeColor1() {
 		var s1 = document.getElementById("s1");
-var p1 = document.getElementById("p1");
-var s2 = document.getElementById("s2");
-var p2 = document.getElementById("p2");
+		var p1 = document.getElementById("p1");
+		var s2 = document.getElementById("s2");
+		var p2 = document.getElementById("p2");
 		s1.style.color = 'blue';
 		p1.style.color = 'blue';
 		s2.style.color = 'rgb(177, 156, 156)';
@@ -13,10 +13,10 @@ var p2 = document.getElementById("p2");
 		
 }
 function changeColor2() {
-	var s1 = document.getElementById("s1");
-var p1 = document.getElementById("p1");
-var s2 = document.getElementById("s2");
-var p2 = document.getElementById("p2");
+		var s1 = document.getElementById("s1");
+		var p1 = document.getElementById("p1");
+		var s2 = document.getElementById("s2");
+		var p2 = document.getElementById("p2");
 		s1.style.color = 'rgb(177, 156, 156)';
 		p1.style.color = 'rgb(177, 156, 156)';
 		s2.style.color = 'blue';
